5f195fb7b72f5c18a21382bef8572837c8f98cee
agus-marmor <amarmor06@gmail.com>
2025-12-20 20:30:24 -0500

    Trying to fix sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://marmorslab.dev/sitemap.xml',
+        sitemap: 'https://www.marmorslab.dev/sitemap.xml',
     }
 }
